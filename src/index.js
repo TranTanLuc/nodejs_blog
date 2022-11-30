be7c99983cacd91733bdf179e8e939c858a0ec15
Tran Tan Luc <luc972000@gmail.com>
2022-11-30 16:31:51 +0700

    MVC

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const { engine } = require("express-handlebars");
 const app = express();
 const path = require("path");
 const port = 3000;
+const route = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -20,13 +28,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
